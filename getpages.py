c32d713c32b77ae14bb3bb44f61ba92dd15f8294
ninja33 <zhenyu.huang@qq.com>
2016-07-13 21:42:34 +0800

    some changes to get baidu pics

--- getpages.py
+++ getpages.py
@@ -1,30 +1,32 @@
 #coding=utf-8
- 
-import urllib2
-from ntlm import HTTPNtlmAuthHandler
+
+import urllib 
+#import urllib2
+#from ntlm import HTTPNtlmAuthHandler
 import re
  
 def downloadPage(url):
-    user = 'user'
-    password = "password"
+    #user = 'user'
+    #password = "password"
 
-    passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-    passman.add_password(None, url, user, password)
+    #passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    #passman.add_password(None, url, user, password)
     #proxy = urllib2.ProxyHandler({'http': 'user:pass@proxy.com:8080'})
     #auth = urllib2.HTTPBasicAuthHandler()
-    auth_NTLM = HTTPNtlmAuthHandler.HTTPNtlmAuthHandler(passman)
-    opener = urllib2.build_opener(auth_NTLM)
-    urllib2.install_opener(opener)
-    return urllib2.urlopen(url).read()
+    #auth_NTLM = HTTPNtlmAuthHandler.HTTPNtlmAuthHandler(passman)
+    #opener = urllib2.build_opener(auth_NTLM)
+    #urllib2.install_opener(opener)
+    return urllib.urlopen(url).read()
  
 def downloadImg(content):
-    print content
     pattern = r'src="(.+?\.jpg)" pic_ext'
     m = re.compile(pattern)
     urls = re.findall(m, content)
-    print urls
     for i, url in enumerate(urls):
         urllib.urlretrieve(url, "%s.jpg" % (i, ))
+        if i > 5 :
+            break
+        
  
 content = downloadPage("http://tieba.baidu.com/p/2460150866")
 downloadImg(content)
