24b1b7ba113133787e123fd41b3fec80ea353f7d
ninja huang <zhenyu.huang@qq.com>
2016-07-15 17:27:04 +0800

    Update getdict.py

--- getdict.py
+++ getdict.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 fr = open('words.txt', 'r')
-fw = open('words_out.txt', 'r')
+fw = open('words_out.txt', 'w')
 for f in fr:
     fw.write(f + '\t' + BeautifulSoup(requests.get('http://dict.cn/' + f).text).selector(.basic).get_text())
 fr.close()
