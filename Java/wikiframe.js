b64c8fb50fcc9508f91d71ac2e1c6d00217a77b5
ninja huang <zhenyu.huang@qq.com>
2017-04-07 15:23:07 +0800

    Update wikiframe.js

--- Java/wikiframe.js
+++ Java/wikiframe.js
@@ -1,7 +1,9 @@
 (function(){
 
+	// the minimum version of jQuery we want
 	var v = "1.3.2";
 
+	// check for jQuery. if it exists, verify it's not too old.
 	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
 		var done = false;
 		var script = document.createElement("script");
@@ -37,17 +39,17 @@
 					var s = prompt("Forget something?");
 				}
 				if ((s != "") && (s != null)) {
-					$("body").append("
-					<div id='wikiframe'>
-						<div id='wikiframe_veil' style=''>
-							<p>Loading...</p>
-						</div>
-						<iframe src='http://en.wikipedia.org/w/index.php?&search="+s+"' onload="$('#wikiframe iframe').slideDown(500);">Enable iFrames.</iframe>
-						<style type='text/css'>
-							#wikiframe_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }
-							#wikiframe_veil p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }
-							#wikiframe iframe { display: none; position: fixed; top: 10%; left: 10%; width: 80%; height: 80%; z-index: 999; border: 10px solid rgba(0,0,0,.5); margin: -5px 0 0 -5px; }
-						</style>
+					$("body").append("\
+					<div id='wikiframe'>\
+						<div id='wikiframe_veil' style=''>\
+							<p>Loading...</p>\
+						</div>\
+						<iframe src='http://en.wikipedia.org/w/index.php?&search="+s+"' onload=\"$('#wikiframe iframe').slideDown(500);\">Enable iFrames.</iframe>\
+						<style type='text/css'>\
+							#wikiframe_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }\
+							#wikiframe_veil p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }\
+							#wikiframe iframe { display: none; position: fixed; top: 10%; left: 10%; width: 80%; height: 80%; z-index: 999; border: 10px solid rgba(0,0,0,.5); margin: -5px 0 0 -5px; }\
+						</style>\
 					</div>");
 					$("#wikiframe_veil").fadeIn(750);
 				}
