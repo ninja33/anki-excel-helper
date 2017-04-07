9d13f1b9ed38699bcbe30d23c826d723367f8c04
ninja huang <zhenyu.huang@qq.com>
2017-04-07 15:24:23 +0800

    Update wikiframe.js

--- Java/wikiframe.js
+++ Java/wikiframe.js
@@ -7,7 +7,7 @@
 	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
 		var done = false;
 		var script = document.createElement("script");
-		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+		script.src = "https://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
 		script.onload = script.onreadystatechange = function(){
 			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
 				done = true;
