dbe042f9f98bc865b1d73e04d763025cfa1afaf1
ninja huang <zhenyu.huang@qq.com>
2017-04-08 10:50:36 +0800

    Update buttonframe.js

--- Java/buttonframe.js
+++ Java/buttonframe.js
@@ -1,18 +1,38 @@
-function getSelText() {
-    var s;
-    try {
-        s=((window.getSelection && window.getSelection()) || (document.getSelection && document.getSelection()) || (document.selection && document.selection.createRange && document.selection.createRange().text));
-    } catch(e){
-        s="nothing selected";
-    }    
-    return s;
+var selectedText;
+
+var selectionEndTimeout = null;
+
+// bind selection change event to my function
+document.onselectionchange = userSelectionChanged;
+
+function userSelectionChanged() {
+    // wait 500 ms after the last selection change event
+    if (selectionEndTimeout) {
+        clearTimeout(selectionEndTimeout);
+        $('.log ol').append('<li>User Selection Changed</li>');
+        // scroll to bottom of the div
+        $('.log').scrollTop($('.log ol').height());
+    }
+    selectionEndTimeout = setTimeout(function () {
+        $(window).trigger('selectionEnd');
+    }, 500);
 }
 
-function selectText()
-{
-    var s = "";
-    s = getSelText().toString();
-    alert(s);
+
+// helper function
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+
+function showSelectedText(){
+    alert(selectedText);
 }
 
 (function(){
@@ -37,11 +57,25 @@ function selectText()
     }
     
     function initMyBookmarklet() {
+        $(window).bind('selectionEnd', function () {
+            // reset selection timeout
+            selectionEndTimeout = null;
+
+            // TODO: Do your cool stuff here........
+
+            // get user selection
+            selectedText = getSelectionText();
+            // if the selection is not empty show it :)
+            if(selectedText != ''){
+                // scroll to bottom of the div
+            }
+        });
+        
         (window.myBookmarklet = function() {
             $("body").append("\
             <div id='wikiframe'>\
                 <div id='wikiframe_veil' style=''>\
-                    <p><img src=\"http://icons.iconarchive.com/icons/martz90/circle-addon1/128/text-plus-icon.png\" onClick=\"selectText()\"></p>\
+                    <p><img src=\"http://icons.iconarchive.com/icons/martz90/circle-addon1/128/text-plus-icon.png\" onClick=\"showSelectedText()\"></p>\
                 </div>\
                 <style type='text/css'>\
                     #wikiframe_veil { display: block; position: fixed; width: 50; height: 50; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }\
